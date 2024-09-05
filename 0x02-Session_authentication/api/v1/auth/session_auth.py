4bb72c8d3ec5c4b8d5c877f09ae3ed952bf66a9b
n-elmasry <almassrynada411@gmail.com>
2024-09-06 01:38:15 +0500

    task 8

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -28,3 +28,16 @@ class SessionAuth(Auth):
         session = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session)
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """ deletes the user session / logout"""
+        if request == None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        if self.user_id_for_session_id(session_id) is None:
+            return False
+
+        del self.user_id_by_session_id[session_id]
+        return True
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -2,14 +2,14 @@
 """ Flask view that handles all routes for the Session authentication."""
 from flask import request, make_response
 from api.v1.views import app_views
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from models.user import User
 from os import getenv
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def session_login() -> str:
-    """session"""
+    """session login"""
     email = request.form.get('email')
     if email is None or email == '':
         return jsonify({"error": "email missing"}), 400
@@ -35,3 +35,15 @@ def session_login() -> str:
     response = make_response(jsonify(user.to_json()))
     response.set_cookie(cookie_name, session_id)
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def session_logout():
+    """session logout"""
+    from api.v1.app import auth
+
+    delete = auth.destroy_session(request)
+
+    if delete is False:
+        abort(404)
+    return jsonify({}), 200
