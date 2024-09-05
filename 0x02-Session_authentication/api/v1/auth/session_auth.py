fbd3da5b473a07ce8474ea0375d754bebf7bf16d
n-elmasry <almassrynada411@gmail.com>
2024-09-05 22:20:53 +0500

    task6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """SessionAuth"""
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -21,3 +22,9 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """(overload) that returns a User instance based on a cookie value"""
+        session = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session)
+        return User.get(user_id)
