af9bd6b084dbf9f343b1e819fe51e4cbdd49760f
n-elmasry <almassrynada411@gmail.com>
2024-09-11 18:13:17 +0800

    task 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,7 +33,11 @@ class DB:
         """Add new user to database
         Returns a User object
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
         return new_user
