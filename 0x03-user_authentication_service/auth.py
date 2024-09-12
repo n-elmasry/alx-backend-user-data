85c824384b3468c01ce2c4067dba5384492815e4
n-elmasry <almassrynada411@gmail.com>
2024-09-12 18:06:10 +0800

    task 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -64,9 +64,10 @@ class Auth:
             return None
         try:
             user = self._db.find_user_by(session_id=session_id)
+            return user.email
         except NoResultFound:
             return None
-        return user
+        # return user
 
     def destroy_session(self, user_id: int) -> None:
         """"updates the corresponding user’s session ID to None."""
