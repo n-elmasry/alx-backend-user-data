2a60d1ee49ec45a6fc0ac97b0136a69665aeadb5
n-elmasry <almassrynada411@gmail.com>
2024-09-12 18:08:17 +0800

    task 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -58,15 +58,17 @@ class Auth:
         except NoResultFound:
             return None
 
-    def get_user_from_session_id(self, session_id: str) -> str:
-        """Takes single session_id string argument
-        Returns corresponding User or None
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Retrieves a user based on a given session ID.
         """
+        user = None
+        if session_id is None:
+            return None
         try:
             user = self._db.find_user_by(session_id=session_id)
-            return user.email
         except NoResultFound:
             return None
+        return user
 
     def destroy_session(self, user_id: int) -> None:
         """"updates the corresponding user’s session ID to None."""
