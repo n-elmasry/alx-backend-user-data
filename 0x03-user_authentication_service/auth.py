7807b94726ad0761d3a9ab3aa4d88f778901ee6b
n-elmasry <almassrynada411@gmail.com>
2024-09-12 23:56:20 +0800

    task 18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -95,7 +95,9 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
-            raise ValueError
+            user = None
+        if user is None:
+            raise ValueError()
         new_hashed_password = _hash_password(password)
         self._db.update_user(
             user.id, hashed_password=new_hashed_password, reset_token=None)
