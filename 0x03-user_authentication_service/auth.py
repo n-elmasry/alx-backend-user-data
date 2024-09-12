bb72cd131d85eef1b8b1dab7ee70885dba8c45eb
n-elmasry <almassrynada411@gmail.com>
2024-09-12 11:54:22 +0800

    task 4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""auth"""
+from bcrypt import hashpw, gensalt
+
+
+def _hash_password(password: str) -> bytes:
+    """ return salted hash of the input password """
+    return hashpw(password.encode('utf-8'), gensalt())
